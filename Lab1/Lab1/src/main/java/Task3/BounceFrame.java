a7a1193021ee403c92b57f8e3dc229d9858a13e8
Oleksandr <OlexandrTanasienko@gmail.com>
2023-02-22 03:54:08 +0200

    Add first experiment with balls

--- Lab1/Lab1/src/main/java/Task3/BounceFrame.java
+++ Lab1/Lab1/src/main/java/Task3/BounceFrame.java
@@ -20,6 +20,14 @@ public class BounceFrame extends JFrame {
         JButton buttonBlue = new JButton("Blue");
         JButton buttonStop = new JButton("Stop");
 
+        JPanel experimentsPanel = new JPanel();
+        experimentsPanel.setBackground(Color.lightGray);
+        JButton experiment1 = new JButton(("1R 1B"));
+        JButton experiment2 = new JButton(("1R 10B"));
+        JButton experiment3 = new JButton(("1R 100B"));
+        JButton experiment4 = new JButton(("1R 1000B"));
+        JButton experiment5 = new JButton(("1R 3000B"));
+
         buttonRed.addActionListener(e -> {
             for(int i = 0; i < 100; i++)
             {
@@ -27,6 +35,7 @@ public class BounceFrame extends JFrame {
                 canvas.add(b);
 
                 BallThread thread = new BallThread(b);
+                thread.setPriority(8);
                 thread.start();
             }
         });
@@ -38,16 +47,40 @@ public class BounceFrame extends JFrame {
                 canvas.add(b);
 
                 BallThread thread = new BallThread(b);
+                thread.setPriority(4);
                 thread.start();
             }
         });
 
         buttonStop.addActionListener(e -> System.exit(0));
 
+        experiment1.addActionListener(e -> {
+            Ball redBall = new Ball(canvas, Color.red);
+            canvas.add(redBall);
+
+            BallThread redBallThread = new BallThread(redBall);
+            redBallThread.setPriority(8);
+            redBallThread.start();
+
+            Ball blueBall = new Ball(canvas, Color.blue);
+            canvas.add(blueBall);
+
+            BallThread blueBallThread = new BallThread(redBall);
+            blueBallThread.setPriority(4);
+            blueBallThread.start();
+        });
+
         buttonPanel.add(buttonRed);
         buttonPanel.add(buttonBlue);
         buttonPanel.add(buttonStop);
 
+        experimentsPanel.add(experiment1);
+        experimentsPanel.add(experiment2);
+        experimentsPanel.add(experiment3);
+        experimentsPanel.add(experiment4);
+        experimentsPanel.add(experiment5);
+
         content.add(buttonPanel, BorderLayout.SOUTH);
+        content.add(experimentsPanel, BorderLayout.NORTH);
     }
 }
