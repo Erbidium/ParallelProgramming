a905fb39562aea3b587bd13d263fbb5fb3f5d477
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-16 20:05:44 +0300

    Add matrices comparator

---
+++ Lab2/lab2/src/main/java/org/example/MatrixComparer.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MatrixComparer {
+    public boolean MatricesAreEqual(float [][] matrixA, float [][] matrixB)
+    {
+        if (matrixA.length != matrixB.length || matrixA[0].length != matrixB[0].length) {
+            return false;
+        }
+
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[0].length; j++) {
+                if (matrixA[i][j] != matrixB[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
