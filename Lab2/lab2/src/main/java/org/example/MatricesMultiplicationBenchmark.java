f166dcdbbcd37b8aee360ee696635c03b6a7b56b
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-18 15:49:04 +0300

    Implement benchmark class

--- Lab2/lab2/src/main/java/org/example/MatricesMultiplicationBenchmark.java
+++ Lab2/lab2/src/main/java/org/example/MatricesMultiplicationBenchmark.java
@@ -21,16 +21,48 @@ public class MatricesMultiplicationBenchmark {
         this.attemptsCount = attemptsCount;
     }
 
-    public float[][] Run()
-    {
+    public float[][] Run() throws Exception {
         var benchmarkResult = new float[matrixSizes.length][threadsCount.length];
 
         for (int i = 0; i < matrixSizes.length; i++)
         {
+            var matrixA = MatrixGenerator.GenerateRandomMatrix(matrixSizes[i]);
+            var matrixB = MatrixGenerator.GenerateRandomMatrix(matrixSizes[i]);
+
             for (int j = 0; j < threadsCount.length; j++)
             {
+                if (matrixMultiplier instanceof IThreadsMultiplier)
+                {
+                    ((IThreadsMultiplier) matrixMultiplier).setThreads(threadsCount[j]);
+                }
 
+                float attemptsSum = 0;
+
+                for (int k = 0; k < attemptsCount; k++)
+                {
+                    var startTime = System.currentTimeMillis();
+                    var parallelMultipliedResult = matrixMultiplier.Multiply(matrixA, matrixB);
+                    var endTime = System.currentTimeMillis();
+
+                    var executionTime = endTime - startTime;
+
+                    attemptsSum += executionTime;
+
+                    var sequentialMultiplier = new SequentialMatrixMultiplier();
+                    var sequentialMultipliedResult = sequentialMultiplier.Multiply(matrixA, matrixB);
+
+                    if (MatrixFunctions.MatricesEqual(parallelMultipliedResult.getMatrix(), sequentialMultipliedResult.getMatrix()))
+                    {
+                        throw new Exception("Parallel algorithm wrong computations result");
+                    }
+                }
+
+                var finalExecutionTime = attemptsSum / attemptsCount;
+
+                benchmarkResult[i][j] = finalExecutionTime;
             }
         }
+
+        return benchmarkResult;
     }
 }
