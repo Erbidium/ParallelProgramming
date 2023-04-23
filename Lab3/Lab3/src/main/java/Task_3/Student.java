947bacb948828aca4bda9931738ff92ae7818fa5
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-23 19:57:36 +0300

    Create Student

---
+++ Lab3/Lab3/src/main/java/Task_3/Student.java
@@ -0,0 +1,21 @@
+package Task_3;
+
+public class Student {
+    private String name;
+
+    private String surname;
+
+    Student(String name, String surname)
+    {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
