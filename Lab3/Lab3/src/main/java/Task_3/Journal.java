d5d44c0f3ed0517034fb0a960faa6e8cdbb9dc61
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-23 20:24:28 +0300

    Initialize Journal

--- Lab3/Lab3/src/main/java/Task_3/Journal.java
+++ Lab3/Lab3/src/main/java/Task_3/Journal.java
@@ -1,7 +1,9 @@
 package Task_3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Journal {
     private Map<Group, Map<Student, ArrayList<Integer>>> gradesMap;
@@ -11,9 +13,42 @@ public class Journal {
     public Journal(ArrayList<Student> students)
     {
         this.students = students;
+
+        InitJournal(students);
+    }
+
+    private void InitJournal(ArrayList<Student> students)
+    {
+        gradesMap = new HashMap<>();
+
+        for (var student: students) {
+            var studentGroup = student.getGroup();
+
+            gradesMap.putIfAbsent(studentGroup, new HashMap<>());
+
+            var groupMap = gradesMap.get(studentGroup);
+
+            groupMap.putIfAbsent(student, new ArrayList<>());
+        }
     }
 
     public ArrayList<Student> getStudents() {
         return students;
+    }
+
+    public Set<Student> getStudentsOfGroup(Group group) {
+        return gradesMap.get(group).keySet();
+    }
+
+    public void AddStudentGrade(Student student, int grade)
+    {
+        var groupMap = gradesMap.get(student.getGroup());
+
+        var studentGrades = groupMap.get(student);
+
+        synchronized (studentGrades)
+        {
+            studentGrades.add(grade);
+        }
     }
 }
