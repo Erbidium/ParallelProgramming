b629fb0ce4f327332c68d0f442c7da9190f46450
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-25 21:42:11 +0300

    Add characteristics printing

--- Lab4/Lab4/src/main/java/Task_1/Main.java
+++ Lab4/Lab4/src/main/java/Task_1/Main.java
@@ -7,12 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Folder folder = Folder.fromDirectory(new File("D:/TextFolder"));
 
-        var parallelFrequenciesSearchTask = new ParallelFrequenciesSearchTask();
+        /*var parallelFrequenciesSearchTask = new ParallelFrequenciesSearchTask();
         var forkJoinTime = SearchTaskBenchmark.getExecutionTimeOfSearchTask(folder, parallelFrequenciesSearchTask);
         System.out.println("Fork / join search took " + forkJoinTime + "ms");
 
         var singleThreadedFrequenciesSearchTask = new SingleThreadedFrequenciesSearchTask();
         var singleThreadedTime = SearchTaskBenchmark.getExecutionTimeOfSearchTask(folder, singleThreadedFrequenciesSearchTask);
-        System.out.println("Single thread search took " + singleThreadedTime + "ms");
+        System.out.println("Single thread search took " + singleThreadedTime + "ms");*/
+
+        var parallelFrequenciesSearchTask = new ParallelFrequenciesSearchTask();
+        var frequenciesOfWordLengths = parallelFrequenciesSearchTask.countFrequencies(folder);
+        var wordLengthCharacteristic = new WordLengthCharacteristic(frequenciesOfWordLengths);
+        WordLengthCharacteristicPrinter.print(wordLengthCharacteristic);
     }
 }
--- Lab4/Lab4/src/main/java/Task_1/WordLengthCharacteristicPrinter.java
+++ Lab4/Lab4/src/main/java/Task_1/WordLengthCharacteristicPrinter.java
@@ -3,6 +3,8 @@ package Task_1;
 public class WordLengthCharacteristicPrinter {
     public static void print(WordLengthCharacteristic wordLengthCharacteristic)
     {
-
+        System.out.printf("Mean: %.2f%n", wordLengthCharacteristic.getMean());
+        System.out.printf("Variance: %.2f%n", wordLengthCharacteristic.getVariance());
+        System.out.printf("Standard deviation: %.2f%n", wordLengthCharacteristic.getStandardDeviation());
     }
 }
