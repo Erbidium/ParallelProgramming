6105799a94ab33a9ec38e6691d7ebe15b5e14749
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-25 12:27:20 +0300

    Add starting code from listing

--- Lab4/Lab4/src/main/java/org/example/Main.java
+++ Lab4/Lab4/src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Folder folder = Folder.fromDirectory(new File("D:/TextFolder"));
+        WordCounter wordCounter = new WordCounter();
+        String searchedWord = "synchronized";
+        final int repeatCount = 4;
+        long startTime, stopTime, counts=0, averTime = 0;
+
+        for (int i = 0; i < repeatCount; i++) {
+            startTime = System.currentTimeMillis();
+            counts = wordCounter.countOccurrencesInParallel(folder, searchedWord);
+            stopTime = System.currentTimeMillis();
+            averTime+=stopTime - startTime;
+        }
+
+        System.out.println(counts + " words are fined. Fork / join search took " + averTime/repeatCount+ "ms");
+
+        averTime = 0;
+
+        for (int i = 0; i < repeatCount; i++) {
+            startTime = System.currentTimeMillis();
+            counts = wordCounter.countOccurrencesOnSingleThread(			  				folder,searchedWord)
+            stopTime = System.currentTimeMillis();
+            averTime+=stopTime - startTime;
+        }
+
+        System.out.println(counts + " words are fined. Single thread search took " + averTime/repeatCount+ "ms");
     }
 }
