6326a58a86155e0736f84973dbf9871df8cf3c9d
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-25 20:52:11 +0300

    Add variance and standard deviation

--- Lab4/Lab4/src/main/java/org/example/WordLengthCharacteristic.java
+++ Lab4/Lab4/src/main/java/org/example/WordLengthCharacteristic.java
@@ -6,14 +6,28 @@ import java.util.Map;
 public class WordLengthCharacteristic {
     private double mean;
 
+    private double variance;
+
+    private double standardDeviation;
+
 
     public WordLengthCharacteristic(HashMap <Integer, Integer> frequenciesOfWordLengths)
     {
         mean = calculateMean(frequenciesOfWordLengths);
+        variance = calculateVariance(frequenciesOfWordLengths, mean);
+        standardDeviation = Math.sqrt(variance);
     }
 
     public double getMean() {
         return mean;
+    }
+
+    public double getVariance() {
+        return variance;
+    }
+
+    public double getStandardDeviation() {
+        return standardDeviation;
     }
 
     private static double calculateMean(HashMap <Integer, Integer> frequenciesOfWordLengths)
@@ -26,5 +40,10 @@ public class WordLengthCharacteristic {
         }
 
         return sum / totalWordsCount;
+    }
+
+    private static double calculateVariance(HashMap <Integer, Integer> frequenciesOfWordLengths, double mean)
+    {
+
     }
 }
