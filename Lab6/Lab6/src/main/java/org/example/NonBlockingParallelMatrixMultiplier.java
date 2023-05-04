7d4072c96ebf1ce37108e9142aad9df9e106bc7f
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-05 02:45:56 +0300

    Implement non-blocking version

--- Lab6/Lab6/src/main/java/org/example/NonBlockingParallelMatrixMultiplier.java
+++ Lab6/Lab6/src/main/java/org/example/NonBlockingParallelMatrixMultiplier.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import mpi.MPI;
+import mpi.Request;
 
 import static java.lang.System.exit;
 
@@ -35,61 +36,82 @@ public class NonBlockingParallelMatrixMultiplier {
             extra = matrixSize % workersNumber;
 
             int offset = 0;
+
+            var offsetSends = new Request[workersNumber];
+            var rowsSends = new Request[workersNumber];
+            var subMatrixASends = new Request[workersNumber];
+            var matrixBSends = new Request[workersNumber];
+
             for (destination = 1; destination <= workersNumber; destination++) {
                 int rows = (destination <= extra) ? averow + 1 : averow;
 
-                MPI.COMM_WORLD.Send(new int[] { offset }, 0, 1, MPI.INT, destination, FROM_MASTER);
-                MPI.COMM_WORLD.Send(new int[] { rows }, 0, 1, MPI.INT, destination, FROM_MASTER);
+                offsetSends[destination - 1] = MPI.COMM_WORLD.Isend(new int[] { offset }, 0, 1, MPI.INT, destination, FROM_MASTER);
+                rowsSends[destination - 1] =  MPI.COMM_WORLD.Isend(new int[] { rows }, 0, 1, MPI.INT, destination, FROM_MASTER);
 
                 var subMatrixA = MatrixFunctions.GetSubMatrix(offset, rows, matrixA);
 
-                MPI.COMM_WORLD.Send(subMatrixA, 0, rows, MPI.OBJECT, destination, FROM_MASTER);
-                MPI.COMM_WORLD.Send(matrixB, 0, matrixSize, MPI.OBJECT, destination, FROM_MASTER);
+                subMatrixASends[destination - 1] = MPI.COMM_WORLD.Isend(subMatrixA, 0, rows, MPI.OBJECT, destination, FROM_MASTER);
+                matrixBSends[destination - 1] = MPI.COMM_WORLD.Isend(matrixB, 0, matrixSize, MPI.OBJECT, destination, FROM_MASTER);
 
                 offset += rows;
             }
+
+            Request.Waitall(offsetSends);
+            Request.Waitall(rowsSends);
+            Request.Waitall(subMatrixASends);
+            Request.Waitall(matrixBSends);
 
             var offsetBuffer = new int[1];
             var rowsBuffer = new int[1];
 
             for (source = 1; source <= workersNumber; source++) {
-                MPI.COMM_WORLD.Recv(offsetBuffer, 0, 1, MPI.INT, source, FROM_WORKER);
-                MPI.COMM_WORLD.Recv(rowsBuffer, 0, 1, MPI.INT, source, FROM_WORKER);
+                var offsetReceive = MPI.COMM_WORLD.Irecv(offsetBuffer, 0, 1, MPI.INT, source, FROM_WORKER);
+                var rowsReceive = MPI.COMM_WORLD.Irecv(rowsBuffer, 0, 1, MPI.INT, source, FROM_WORKER);
+
+                rowsReceive.Wait();
 
                 var calculatedSubMatrixC = new int[rowsBuffer[0]][matrixSize];
-                MPI.COMM_WORLD.Recv(calculatedSubMatrixC, 0, rowsBuffer[0], MPI.OBJECT, source, FROM_WORKER);
+                var calculatedSubMatrixReceive = MPI.COMM_WORLD.Irecv(calculatedSubMatrixC, 0, rowsBuffer[0], MPI.OBJECT, source, FROM_WORKER);
 
+                Request.Waitall(new Request[] { offsetReceive, calculatedSubMatrixReceive });
                 MatrixFunctions.AddSubMatrix(c, calculatedSubMatrixC, offsetBuffer[0]);
             }
 
             var endTime = MPI.Wtime();
 
-            //System.out.println(endTime - startTime);
+            System.out.println(endTime - startTime);
 
-            System.out.println("****\n");
-            System.out.println("Result Matrix:\n");
-            MatrixPrinter.Print(c);
-            System.out.println("\n********\n");
-            System.out.println("Done.\n");
+            //System.out.println("****\n");
+            //System.out.println("Result Matrix:\n");
+            //MatrixPrinter.Print(c);
+            //System.out.println("\n********\n");
+            //System.out.println("Done.\n");
         }
         else {
             var offset = new int[1];
             var rows = new int[1];
 
-            MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, MASTER, FROM_MASTER);
+            var offsetReceive = MPI.COMM_WORLD.Irecv(offset, 0, 1, MPI.INT, MASTER, FROM_MASTER);
+            var rowsReceive = MPI.COMM_WORLD.Irecv(rows, 0, 1, MPI.INT, MASTER, FROM_MASTER);
+
+            offsetReceive.Wait();
+            var offsetSend = MPI.COMM_WORLD.Isend(offset, 0, 1, MPI.INT, MASTER, FROM_WORKER);
+
+            rowsReceive.Wait();
 
             var workerMatrixAPart = new int[rows[0]][];
             var workerMatrixB = new int[matrixSize][];
 
-            MPI.COMM_WORLD.Recv(workerMatrixAPart, 0, rows[0], MPI.OBJECT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(workerMatrixB, 0, matrixSize, MPI.OBJECT, MASTER, FROM_MASTER);
+            var matrixAPartReceive = MPI.COMM_WORLD.Irecv(workerMatrixAPart, 0, rows[0], MPI.OBJECT, MASTER, FROM_MASTER);
+            var matrixBReceive = MPI.COMM_WORLD.Irecv(workerMatrixB, 0, matrixSize, MPI.OBJECT, MASTER, FROM_MASTER);
 
+            Request.Waitall(new Request[] { matrixAPartReceive, matrixBReceive });
             var calculatedRows = MatrixFunctions.Multiply(workerMatrixAPart, workerMatrixB);
 
-            MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, MASTER, FROM_WORKER);
-            MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, MASTER, FROM_WORKER);
-            MPI.COMM_WORLD.Send(calculatedRows, 0, rows[0], MPI.OBJECT, MASTER, FROM_WORKER);
+            var rowsSend = MPI.COMM_WORLD.Isend(rows, 0, 1, MPI.INT, MASTER, FROM_WORKER);
+            var calculatedRowsSend = MPI.COMM_WORLD.Isend(calculatedRows, 0, rows[0], MPI.OBJECT, MASTER, FROM_WORKER);
+
+            Request.Waitall(new Request[] { offsetSend, rowsSend, calculatedRowsSend });
         }
 
         MPI.Finalize();
