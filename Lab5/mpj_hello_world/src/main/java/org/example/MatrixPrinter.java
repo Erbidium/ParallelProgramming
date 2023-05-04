3f4dbb7fe851c923d9c29690980f4387422660eb
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-04 14:39:17 +0300

    Add matrix printing

---
+++ Lab5/mpj_hello_world/src/main/java/org/example/MatrixPrinter.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MatrixPrinter {
+    public static void Print(int[][] matrix)
+    {
+        for (int[] row : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(row[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
