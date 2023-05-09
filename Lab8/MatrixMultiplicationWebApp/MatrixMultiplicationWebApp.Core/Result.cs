14b69649b2403d271a86cbe082387b1b27b2c6fb
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-10 00:52:07 +0300

    Add Result class

---
+++ Lab8/MatrixMultiplicationWebApp/MatrixMultiplicationWebApp.Core/Result.cs
@@ -0,0 +1,44 @@
+﻿namespace MatrixMultiplicationWebApp.Core;
+
+public class Result {
+    private int[][] matrix;
+
+    public Result(int matrixSize)
+    {
+        matrix = new int[matrixSize][];
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            matrix[i] = new int[matrixSize];
+        }
+    }
+
+    public Result(int[][] matrix)
+    {
+        this.matrix = matrix;
+    }
+
+    public int[][] GetMatrix() {
+        return matrix;
+    }
+
+    public void WriteValueToCell(int cellValue, int row, int column)
+    {
+        matrix[row][column] = cellValue;
+    }
+
+    public void AddValueToCell(int cellValue, int row, int column)
+    {
+        matrix[row][column] += cellValue;
+    }
+
+    public void AddSubMatrix(int[][] subMatrix, int row, int column)
+    {
+        for (int i = 0; i < subMatrix.Length; i++)
+        {
+            for (int j = 0; j < subMatrix[0].Length; j++)
+            {
+                matrix[row + i][column + j] += subMatrix[i][j];
+            }
+        }
+    }
+}
