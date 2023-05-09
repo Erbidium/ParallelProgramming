7f21421d726f69ee2dfa0a7a1d10189e09f34a24
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-10 01:19:01 +0300

    Add matrix functions

---
+++ Lab8/MatrixMultiplicationWebApp/MatrixMultiplicationWebApp.Core/MatrixFunctions.cs
@@ -0,0 +1,23 @@
+﻿namespace MatrixMultiplicationWebApp.Core;
+
+public static class MatrixFunctions
+{
+    public static int[][] GetTransposed(int [][] matrix)
+    {
+        var rows = matrix.Length;
+        var columns = matrix[0].Length;
+
+        var transposedMatrix = new int[columns][];
+
+        for(int i = 0; i < rows; i++)
+        {
+            transposedMatrix[i] = new int[rows];
+            for (int j = 0; j < columns; j++)
+            {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+}
