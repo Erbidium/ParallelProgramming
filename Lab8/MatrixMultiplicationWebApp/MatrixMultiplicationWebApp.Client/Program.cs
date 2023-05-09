b286fabb42ff808bf2c895dd2315ca41fc4edd6e
Oleksandr <OlexandrTanasienko@gmail.com>
2023-05-10 02:16:35 +0300

    Implement simple client with post request

--- Lab8/MatrixMultiplicationWebApp/MatrixMultiplicationWebApp.Client/Program.cs
+++ Lab8/MatrixMultiplicationWebApp/MatrixMultiplicationWebApp.Client/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+using var client = new HttpClient();
+
+var response = await client.PostAsync($"http://localhost:5172/WeatherForecast/multiply-random-matrices/{20}", null);
+if (response.IsSuccessStatusCode)
+{
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine($"Error: {response.StatusCode}");
+}
